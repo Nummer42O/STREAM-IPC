19de78a54fbdfd08006185211f0a8950ed5f6eae
KARTOFF8xE <georgmuck1@gmail.com>
2025-05-23 14:59:37 +0000

    refactored a bit

--- include/ipc/datastructs/information-datastructs.hpp
+++ include/ipc/datastructs/information-datastructs.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "ipc/common.hpp"
+#include "ipc/datastructs/sharedMem-datastructs.hpp"
+
+#include <chrono>
 
 
 #define MSG_TYPE_NODE_REQUEST GET_COUNTER
@@ -83,9 +86,9 @@ struct NodeTimerToUpdate
 #define MSG_TYPE_NODE_STATE_UPDATE GET_COUNTER
 struct NodeStateUpdate
 {
-  primaryKey_t  primaryKey;
-  u_int64_t     state;  // TODO: CHANGE to according ENUM!!!
-  bool          isUpdate = true;
+  primaryKey_t      primaryKey;
+  sharedMem::State  state;
+  bool              isUpdate = true;
 };
 
 #define MSG_TYPE_NODE_RESPONSE GET_COUNTER
@@ -282,4 +285,128 @@ struct SHMAddressResponse
 {
   requestId_t requestID;
   MAKE_STRING (memAddress);
+};
+
+enum class MessageType {
+    NONE,
+    CPU,
+    MEMORY,
+    DISK,
+    NODETRACE,
+    PUBLISHERTRACE,
+    SUBSCRIBERTRACE,
+    SERVICETRACE,
+    CLIENTTRACE,
+    TIMERTRACE,
+    STATEMACHINEINITTRACE,
+    STATETRANSITIONTRACE
+};
+
+struct InputValue {
+    std::chrono::nanoseconds timestamp;
+    primaryKey_t primaryKey;
+    long double value;
+    MessageType type;
+};
+
+enum ResponseType {
+    NUMERICAL,
+    TEXTUAL,
+};
+
+struct ResponseHeader {
+    ResponseType    type;
+};
+
+struct NumericalResponse {
+    size_t number;
+    size_t total;
+    double value;
+};
+
+struct TextualResponse {
+    size_t number;
+    size_t total;
+    MAKE_STRING(line);
+};
+
+struct Response {
+    ResponseHeader          header;
+    union {
+        NumericalResponse   numerical;
+        TextualResponse     textual;
+    };
+};
+
+struct TraceHeader {
+    MessageType type;
+
+    TraceHeader (MessageType type) { this->type = type; }
+};
+
+struct Node {
+    MAKE_STRING (name);
+    MAKE_STRING (nspace);
+    u_int64_t   handle;
+    pid_t       pid;
+    u_int32_t   stateChangeTime;    
+};
+
+struct Publisher {
+    MAKE_STRING (topicName);
+    u_int64_t   nodeHandle;
+};
+
+struct Subscriber {
+    MAKE_STRING (topicName);
+    u_int64_t   nodeHandle;
+};
+
+struct Service {
+    MAKE_STRING (name);
+    u_int64_t   nodeHandle;
+};
+
+struct Client {
+    MAKE_STRING (srvName);
+    u_int64_t   nodeHandle;
+};
+
+struct Timer {
+    u_int64_t   nodeHandle;
+    u_int32_t   frequency;
+};
+
+struct LifecycleSMInit {
+    u_int64_t   nodeHandle;
+    u_int64_t   stateMachine;
+};
+
+enum LifeCycleState {
+    INVALID,
+    UNCONFIGURED,
+    INACTIVE,
+    ACTIVE,
+    FINALIZED,
+};
+
+struct LifecycleTransition {
+    u_int64_t       stateMachine;
+    LifeCycleState  state;
+};
+
+struct TraceMessage {
+    TraceHeader header;
+    union {
+        Node                node;
+        Publisher           publisher;
+        Subscriber          subscriber;
+        Service             service;
+        Client              client;
+        Timer               timer;
+        LifecycleSMInit     lcSmInit;
+        LifecycleTransition lcTrans;
+    };
+
+    TraceMessage (MessageType type) : header (type) {}
 };
---
+++ include/ipc/datastructs/sharedMem-datastructs.hpp
@@ -0,0 +1,134 @@
+#pragma once
+
+#include "ipc/common.hpp"
+
+#include <chrono>
+
+
+namespace sharedMem {
+
+enum class MessageType {
+    NONE,
+    CPU,
+    MEMORY,
+    DISK,
+    NODETRACE,
+    PUBLISHERTRACE,
+    SUBSCRIBERTRACE,
+    SERVICETRACE,
+    CLIENTTRACE,
+    TIMERTRACE,
+    STATEMACHINEINITTRACE,
+    STATETRANSITIONTRACE
+};
+
+struct InputValue {
+    std::chrono::nanoseconds timestamp;
+    primaryKey_t primaryKey;
+    long double value;
+    MessageType type;
+};
+
+enum ResponseType {
+    NUMERICAL,
+    TEXTUAL,
+};
+
+struct ResponseHeader {
+    ResponseType    type;
+};
+
+struct NumericalResponse {
+    size_t number;
+    size_t total;
+    double value;
+};
+
+struct TextualResponse {
+    size_t number;
+    size_t total;
+    MAKE_STRING(line);
+};
+
+struct Response {
+    ResponseHeader          header;
+    union {
+        NumericalResponse   numerical;
+        TextualResponse     textual;
+    };
+};
+
+struct TraceHeader {
+    MessageType type;
+
+    TraceHeader (MessageType type) { this->type = type; }
+};
+
+struct Node {
+    MAKE_STRING (name);
+    MAKE_STRING (nspace);
+    u_int64_t   handle;
+    pid_t       pid;
+    u_int32_t   stateChangeTime;    
+};
+
+struct Publisher {
+    MAKE_STRING (topicName);
+    u_int64_t   nodeHandle;
+};
+
+struct Subscriber {
+    MAKE_STRING (topicName);
+    u_int64_t   nodeHandle;
+};
+
+struct Service {
+    MAKE_STRING (name);
+    u_int64_t   nodeHandle;
+};
+
+struct Client {
+    MAKE_STRING (srvName);
+    u_int64_t   nodeHandle;
+};
+
+struct Timer {
+    u_int64_t   nodeHandle;
+    u_int32_t   frequency;
+};
+
+struct LifecycleSMInit {
+    u_int64_t   nodeHandle;
+    u_int64_t   stateMachine;
+};
+
+enum State {
+    INVALID,
+    UNCONFIGURED,
+    INACTIVE,
+    ACTIVE,
+    FINALIZED,
+};
+
+struct LifecycleTransition {
+    u_int64_t   stateMachine;
+    State       state;
+};
+
+struct TraceMessage {
+    TraceHeader header;
+    union {
+        Node                node;
+        Publisher           publisher;
+        Subscriber          subscriber;
+        Service             service;
+        Client              client;
+        Timer               timer;
+        LifecycleSMInit     lcSmInit;
+        LifecycleTransition lcTrans;
+    };
+
+    TraceMessage (MessageType type) : header (type) {}
+};
+
+}
